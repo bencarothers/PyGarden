23fbc7950d282eb0e7a00ca2b47729802efff42f
bencarothers <bentcar@gmail.com>
2015-04-02 12:16:14 -0400

    commented GardenTweet

--- GardenTweet.py
+++ GardenTweet.py
@@ -1,9 +1,13 @@
 #******************************************#
 # Twitter-Enabled Garden Watering
-# *****************************************#
+# Author: Ben Carothers
+#******************************************#
+
+
 import twitter
 import time
 
+#Connect to the Garden's Twitter account
 api = twitter.Api(consumer_key='Z6tyBKfbWTIkbXX1XeOTU3qGF',
                   consumer_secret='MwVDyHYq4Ria4ub3pAmq7D38dgAEtCJKCVgKOMkBimaJpRkZwp',
                   access_token_key='3119886269-GQfctpmBOpY3wodDxO69SoRdDkCRBYNVjiFOcO0',
@@ -17,12 +21,12 @@ def waterWatcher():
 		lastId = status[0].id;
 		tweets = [s.text for s in status]
 		drip = tweets[0].split()
-		if drip[0] == '#waterMe':
-			print 'water'
-			api.DestroyStatus(lastId)
+		if drip[0] == '#waterMe':               #if #waterMe is found in the tweet 
+                    print 'water'                       #TODO: look for water arguments and send them to OSPI
+			api.DestroyStatus(lastId)       # Destroy the tweet
 		elif drip[0] != '#waterMe':		
 			print 'delete'
-			api.DestroyStatus(lastId)
+			api.DestroyStatus(lastId)       #This is a garbage tweet (no water commands) so delete it
 	else:
 		print 'no tweets'
 
