02e16e6103078873b52fb24e3e3506da3dc018aa
Ben Carothers <ben.carothers13@ncf.edu>
2015-05-20 14:19:17 -0400

    Small edits to the the README

--- GardenTweet.py
+++ GardenTweet.py
@@ -6,6 +6,7 @@
 
 import twitter
 import time
+import re
 import SprinklerGPIO
 
 #Connect to the Garden's Twitter account
@@ -15,23 +16,32 @@ def waterWatcher():
                   access_token_key='3119886269-GQfctpmBOpY3wodDxO69SoRdDkCRBYNVjiFOcO0',
                   access_token_secret='6wBXAhL2WjYMZw97patQZUSbJ2GjNjbkRftflrwldpGrn')
 
+    #Lists all tweets on the user's Timeline
     status = api.GetUserTimeline('X')
 
+                        #---- Represents the number of stations, but I'm currently only operating with one
+                        #|
+    drip = SprinklerGPIO(1)
+
     while len(status) > 0:
+
         lastId = status[0].id
         tweets = [s.text for s in status]
         lastTweet = tweets[0].split()
+
         if lastTweet[0] == '#waterMe':
-            SprinklerGPIO.setStationStatus(1,1)
+            drip.setStationStatus(1,1)
             time.sleep(60)
-            SprinklerGPIO.setStationStatus(1,1)
+            drip.setStationStatus(1,1)
             api.DestroyStatus(lastId)
         elif lastTweet[0] != '#waterMe':
             print 'delete'
             api.DestroyStatus(lastId)
+
     else:
         print 'no tweets'
 
+    time.sleep(15) #Avoid twitter rate limiting
+
 while 1:
 	waterWatcher()
-	time.sleep(15) #Avoid twitter rate limiting
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-**TwiPy Garden
+**TwiPy Garden**
 
-**What**
-
-**How**
+A program written in python that monitors the specified twitter account looking
+for tweets that contain the #waterMe and triggers a drip irrigation valve with
+specified parameters by interfacing with the Open Sprinkler Pi
 
 ## Motivation
+
+I wanted a lightweight program with simple controls to water my garden
 
 ## Installation and Running
 
 ## Selected Libraries Used
+
+Python-Twitter API
+
