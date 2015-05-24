987380e6a386d2e1c6010af9e1c5602e67988293
Ben <benjamin.carothers13@ncf.edu>
2015-05-23 21:02:07 -0400

    Creating more privacy

--- GardenTweet.py
+++ GardenTweet.py
@@ -16,46 +16,44 @@ logging.basicConfig(filename=LOG_FILENAME,
                     level=logging.INFO,
                     )
 
-# Connect to the Garden's Twitter account
+# intantiating a class that creates a connection to the Twitter account
+# using user specific keys and secrets
+
+thirstyGarden = TwitterLogin.thirstyGarden()
+api = thirstyGarden.api
 
 def waterWatcher():
     try:
+        dripController = SprinklerGPIO.SprinklerGPIO(1)
+        tweetsOnTimeline = api.GetUserTimeline('X')
 
-      #TODO move this a file that isn't tracked by git
+        for x in range(len(tweetsOnTimeline)):
+            tweets = [s.text for s in tweetsOnTimeline]
 
-        thirstyGarden = TwitterLogin()
-        api = thirstyGarden.api
+            waterOptions = re.compile(r"#waterMe \d+|#waterMe")
+            foundOptions = waterOptions.search(tweets[0]).group().split(" ")
 
-                                         # -- Represents the number of OSPi stations
-                                         # |
-        drip = SprinklerGPIO.SprinklerGPIO(1)
-
-        #  Lists all tweets on the user's TimeLine
-        status = api.GetUserTimeline('X')
-
-        for x in range(len(status)):
-            tweets = [s.text for s in status]
-
-            gardenRe = re.compile(r"#waterMe \d+|#waterMe")
-            options = gardenRe.search(tweets[0]).group().split(" ")
-
-            if options[0] == '#waterMe':
-                drip.setStationStatus(0,1)
-                minutes = .1 if len(options) < 2 else int(options[1])
-                time.sleep(minutes * 60)
-                drip.setStationStatus(0,0)
-                logging.debug('The garden was watered for %d minutes' % minutes)
+            if foundOptions[0] == '#waterMe':
+                minutes = .1 if len(foundOptions) < 2 else int(foundOptions[1])
+                waterForXMinutes(minutes)
                 #TODO notify user in some way the garden has been watered
-                api.DestroyStatus(status[0].id)
+                api.DestroyStatus(tweetsOnTimeline[0].id)
             else:
                 #TODO Notify the user a junk tweet was deleted
-                api.DestroyStatus(status[0].id)
+                api.DestroyStatus(tweetsOnTimeline[0].id)
         else:
             print 'no tweets'
+
         time.sleep(15)  # Avoid twitter rate limiting
+
     finally:
-        if drip.getStationStatus(0) == 1:
-            drip.setStationStatus(0,0)
+        if dripController.getStationStatus(0) == 1:
+            dripController.setStationStatus(0,0)
+
+def waterForXMinutes(x):
+    dripController.setStationStatus(0,1)
+    time.sleep(minutes * 60)
+    dripController.setStationStatus(0,0)
 
 while 1:
         waterWatcher()
